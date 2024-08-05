e9ba10c28f53c81f1210617c75d1dc891080134f
AbhishekSontakke02 <abhisontakke3930@gmail.com>
2024-08-05 13:07:31 +0530

    first commit

---
+++ src/empWage2/EmpWage2.java
@@ -0,0 +1,8 @@
+package empWage2;
+
+public class EmpWage2 {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Employee Wage Computation Program!");
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module EmployeWage2 {
+}
