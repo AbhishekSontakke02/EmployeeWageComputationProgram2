b1d9b74bc97a16755982a72710a7e4b1ed8163b4
AbhishekSontakke02 <abhisontakke3930@gmail.com>
2024-08-05 14:18:47 +0530

    imlemented UC2

--- src/empWage2/EmpWage2.java
+++ src/empWage2/EmpWage2.java
@@ -2,6 +2,9 @@ package empWage2;
 
 public class EmpWage2 {
 	static int IS_FULL_TIME = 1;
+	static int EMP_RATE_PER_HOUR = 20;
+	static int EMP_HOURS = 0;
+	static int EMP_WAGE = 0;
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to Employee Wage Computation Program");
@@ -11,17 +14,24 @@ public class EmpWage2 {
 		else {
 			System.out.println("Employee is Absent!");
 		}
+		System.out.println("EMP Wage : "+calculateEmpWage());
 		
 	}
 	
 	private static boolean isEmpPresent() {
 		double empCheck = Math.floor(Math.random()*10)%2;
 		if(empCheck == IS_FULL_TIME) {
+			EMP_HOURS = 8;
 			return true;
 		}
 		else {
+			EMP_HOURS = 0;
 			return false;
 		}
+	}
+	private static int calculateEmpWage() {
+		EMP_WAGE = EMP_HOURS * EMP_RATE_PER_HOUR;
+		return EMP_WAGE;
 	}
 
 }
